43067eb15b83eafdd4cf72a08273722ff93803ac
Pham Minh <mhp12092003@gmail.com>
2023-11-09 19:04:27 +0700

    rut gon css cho Login

--- FrontEnd/src/Login/login.js
+++ FrontEnd/src/Login/login.js
@@ -5,17 +5,17 @@ import './Login.css'
 function Login() {
     return (
         <>
-            <form>
-                <div className="UserLabel">User Name</div>
-                <div className="UsernameBox">
-                    <FontAwesomeIcon className="UserIcon" icon={faUser} />
-                    <input className="UsernameInput" placeholder='Username'></input>
+            <form className="FormCointainer">
+                <div className="Label">User Name</div>
+                <div className="BoxWrapper">
+                    <FontAwesomeIcon className="Icon" icon={faUser} />
+                    <input className="Input" placeholder='Username'></input>
                 </div>
 
-                <div className="PasswordLabel">Password</div>
-                <div className="PasswordBox">
-                    <FontAwesomeIcon className="PasswordIcon" icon={faLock} />
-                    <input type="password" className="PasswordInput" placeholder='Password'></input>
+                <div className="Label">Password</div>
+                <div className="BoxWrapper">
+                    <FontAwesomeIcon className="Icon" icon={faLock} />
+                    <input type="password" className="Input" placeholder='Password'></input>
                 </div>
 
                 <button className="LoginBtn" type="submit">Log In</button>
