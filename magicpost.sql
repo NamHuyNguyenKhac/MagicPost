7a10a1bd866a03c0423814797a1bff249d21fbdf
NamHuyNguyenKhac <21020542@vnu.edu.vn>
2023-12-28 23:27:16 +0700

    Update magicpost.sql

--- magicpost.sql
+++ magicpost.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th12 28, 2023 lúc 04:52 PM
+-- Thời gian đã tạo: Th12 28, 2023 lúc 05:26 PM
 -- Phiên bản máy phục vụ: 10.4.24-MariaDB-log
 -- Phiên bản PHP: 8.1.6
 
@@ -39,8 +39,8 @@ CREATE TABLE `gathering_points` (
 --
 
 INSERT INTO `gathering_points` (`id`, `name`, `address`, `employeeId`) VALUES
-(1, 'Điểm tập kết miền Bắc', 'Hà Nội', NULL),
-(2, 'Điểm tập kết miền Trung', 'Đà Nẵng', NULL),
+(1, 'Điểm tập kết miền Bắc', 'Hà Nội', 3),
+(2, 'Điểm tập kết miền Trung', 'Đà Nẵng', 4),
 (3, 'Điểm tập kết miền Nam', 'TP Hồ Chí Minh', NULL);
 
 -- --------------------------------------------------------
@@ -51,9 +51,9 @@ INSERT INTO `gathering_points` (`id`, `name`, `address`, `employeeId`) VALUES
 
 CREATE TABLE `package_info` (
   `id` int(11) NOT NULL,
-  `type` varchar(50) DEFAULT NULL,
+  `type` enum('Tài liệu','Hàng Hóa') DEFAULT NULL,
   `fare` int(11) DEFAULT NULL,
-  `weight` int(11) DEFAULT NULL,
+  `weight` decimal(3,2) DEFAULT NULL,
   `senderAddress` varchar(50) DEFAULT NULL,
   `receiverAddress` varchar(50) DEFAULT NULL,
   `userId` int(11) DEFAULT NULL
@@ -64,18 +64,18 @@ CREATE TABLE `package_info` (
 --
 
 INSERT INTO `package_info` (`id`, `type`, `fare`, `weight`, `senderAddress`, `receiverAddress`, `userId`) VALUES
-(1, 'Tài liệu', 30000, 1, '23B Lê Lợi, Quận Hoàn Kiếm', '72 Pasteur, Quận 3', 15),
-(2, 'Tài liệu', 52000, 1, '45 Điện Biên Phủ, Quận Ba Đình', '15 Bùi Viện, Quận 1', 16),
-(3, 'Hàng hóa', 52000, 6, '78 Hàng Điếu, Quận Hoàn Kiếm', '205 Nguyễn Thị Minh Khai, Quận 3', 18),
-(4, 'Tài liệu', 57000, 0, '124 Lạc Trung, Quận Hai Bà Trưng', '40 Phạm Ngọc Thạch, Quận 3', 25),
-(5, 'Hàng hóa', 48000, 2, '56 Thái Hà, Quận Đống Đa', '88 Nguyễn Huệ, Quận 1', 22),
-(6, 'Tài liệu', 22000, 1, '9 Nguyễn Tri Phương, Quận Hai Bà Trưng', '50 Bạch Đằng, Quận Hải Châu', 24),
-(7, 'Tài liệu', 58000, 1, '33A Phan Đình Phùng, Quận Ba Đình', '89 Trần Phú, Quận Hải Châu', 28),
-(8, 'Hàng hóa', 65000, 3, '72 Đống Đa, Quận Hoàn Kiếm', '102 Nguyễn Chí Thanh, Quận Hải Châu', 14),
-(9, 'Hàng hóa', 54000, 9, '15 Hoàng Hoa Thám, Quận Ba Đình', '15 Lê Duẩn, Quận Hải Châu', 17),
-(10, 'Tài liệu', 44000, 0, '205 Cầu Giấy, Quận Cầu Giấy', '73 Phan Châu Trinh, Quận Hải Châu', 21),
-(11, 'Hàng hóa', 35000, 3, '40 Lê Thánh Tông, Quận Hai Bà Trưng', '50 Bạch Đằng, Quận Hải Châu', 26),
-(12, 'Tài liệu', 72000, 3, '88 Trần Nhật Duật, Quận Hoàn Kiếm', '124 Lạc Trung, Quận Hai Bà Trưng', 30);
+(1, 'Tài liệu', 30000, '2.57', '23B Lê Lợi, Quận Hoàn Kiếm', '72 Pasteur, Quận 3', 15),
+(2, 'Tài liệu', 52000, '1.63', '45 Điện Biên Phủ, Quận Ba Đình', '15 Bùi Viện, Quận 1', 16),
+(3, 'Hàng Hóa', 52000, '1.50', '78 Hàng Điếu, Quận Hoàn Kiếm', '205 Nguyễn Thị Minh Khai, Quận 3', 18),
+(4, 'Tài liệu', 57000, '1.00', '124 Lạc Trung, Quận Hai Bà Trưng', '40 Phạm Ngọc Thạch, Quận 3', 25),
+(5, 'Hàng Hóa', 48000, '0.80', '56 Thái Hà, Quận Đống Đa', '88 Nguyễn Huệ, Quận 1', 22),
+(6, 'Tài liệu', 22000, '1.00', '9 Nguyễn Tri Phương, Quận Hai Bà Trưng', '50 Bạch Đằng, Quận Hải Châu', 24),
+(7, 'Tài liệu', 58000, '0.37', '33A Phan Đình Phùng, Quận Ba Đình', '89 Trần Phú, Quận Hải Châu', 28),
+(8, 'Hàng Hóa', 65000, '1.50', '72 Đống Đa, Quận Hoàn Kiếm', '102 Nguyễn Chí Thanh, Quận Hải Châu', 14),
+(9, 'Hàng Hóa', 54000, '1.29', '15 Hoàng Hoa Thám, Quận Ba Đình', '15 Lê Duẩn, Quận Hải Châu', 17),
+(10, 'Tài liệu', 44000, '0.43', '205 Cầu Giấy, Quận Cầu Giấy', '73 Phan Châu Trinh, Quận Hải Châu', 21),
+(11, 'Hàng Hóa', 35000, '1.14', '40 Lê Thánh Tông, Quận Hai Bà Trưng', '50 Bạch Đằng, Quận Hải Châu', 26),
+(12, 'Tài liệu', 72000, '0.75', '88 Trần Nhật Duật, Quận Hoàn Kiếm', '124 Lạc Trung, Quận Hai Bà Trưng', 30);
 
 -- --------------------------------------------------------
 
@@ -240,7 +240,7 @@ INSERT INTO `transaction_points` (`id`, `name`, `address`, `employeeId`, `gather
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `fullName` varchar(50) NOT NULL,
-  `sex` varchar(50) NOT NULL,
+  `sex` enum('Nam','Nữ') NOT NULL,
   `email` varchar(50) NOT NULL,
   `phoneNumber` varchar(50) NOT NULL,
   `dob` varchar(50) NOT NULL
@@ -253,30 +253,30 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `fullName`, `sex`, `email`, `phoneNumber`, `dob`) VALUES
 (1, 'pro', 'Nam', 'abc@gmail.com', '0123456789', '13-05-2003'),
 (2, 'Quang', 'Nam', 'Quang@gmail.com', '5295930527', '22-11-1997'),
-(3, 'abc', 'nam', 'def@gmail.com', '0123456789', '20-10-2003'),
-(4, 'Tuấn', 'Nam', 'Tuấn@gmail.com', '6087998928', '27-06-1994'),
+(3, 'abc', 'Nam', 'def@gmail.com', '0123456789', '20-10-2003'),
+(4, 'Tuấn', 'Nam', 'Tuan@gmail.com', '6087998928', '27-06-1994'),
 (5, 'Trung', 'Nam', 'Trung@gmail.com', '3069852925', '18-03-1999'),
-(6, 'Đức', 'Nam', 'Đức@gmail.com', '2725778486', '09-08-1993'),
-(7, 'Hoàng', 'Nam', 'Hoàng@gmail.com', '8369350539', '14-02-2002'),
+(6, 'Đức', 'Nam', 'Duc@gmail.com', '2725778486', '09-08-1993'),
+(7, 'Hoàng', 'Nam', 'Hoang@gmail.com', '8369350539', '14-02-2002'),
 (8, 'Anh', 'Nam', 'Anh@gmail.com', '5077225421', '30-12-1995'),
-(9, 'Tùng', 'Nam', 'Tùng@gmail.com', '6728559271', '07-11-1991'),
-(10, 'Thành', 'Nam', 'Thành@gmail.com', '3565229721', '19-05-2004'),
-(11, 'Sơn', 'Nam', 'Sơn@gmail.com', '3593409407', '25-10-1998'),
-(12, 'Dương', 'Nam', 'Dương@gmail.com', '8923515192', '12-05-1990'),
-(13, 'Cường', 'Nam', 'Cường@gmail.com', '0268340341', '09-01-2003'),
-(14, 'Vũ', 'Nam', 'Vũ@gmail.com', '2154201005', '04-09-1996'),
+(9, 'Tung', 'Nam', 'Tùng@gmail.com', '6728559271', '07-11-1991'),
+(10, 'Thành', 'Nam', 'Thanh@gmail.com', '3565229721', '19-05-2004'),
+(11, 'Sơn', 'Nam', 'Son@gmail.com', '3593409407', '25-10-1998'),
+(12, 'Dương', 'Nam', 'Duong@gmail.com', '8923515192', '12-05-1990'),
+(13, 'Cường', 'Nam', 'Cuong@gmail.com', '0268340341', '09-01-2003'),
+(14, 'Vũ', 'Nam', 'Vu@gmail.com', '2154201005', '04-09-1996'),
 (15, 'Nam', 'Nam', 'Nam@gmail.com', '7111841253', '28-11-2000'),
-(16, 'Hương', 'Nữ', 'Hương@gmail.com', '6605098417', '17-08-1992'),
+(16, 'Hương', 'Nữ', 'Huong@gmail.com', '6605098417', '17-08-1992'),
 (17, 'Lan', 'Nữ', 'Lan@gmail.com', '5025009012', '23-01-1994'),
 (18, 'Linh', 'Nữ', 'Linh@gmail.com', '5977283178', '14-06-2001'),
-(19, 'Hà', 'Nữ', 'Hà@gmail.com', '1516378489', '03-07-1997'),
-(20, 'Thảo', 'Nữ', 'Thảo@gmail.com', '5077237733', '29-09-1990'),
-(21, 'Ngọc', 'Nữ', 'Ngọc@gmail.com', '5043384555', '01-12-2005'),
+(19, 'Hà', 'Nữ', 'Ha@gmail.com', '1516378489', '03-07-1997'),
+(20, 'Thao', 'Nữ', 'Thảo@gmail.com', '5077237733', '29-09-1990'),
+(21, 'Ngoc', 'Nữ', 'Ngọc@gmail.com', '5043384555', '01-12-2005'),
 (22, 'Mai', 'Nữ', 'Mai@gmail.com', '6026395741', '05-02-1993'),
-(23, 'Ánh', 'Nữ', 'Ánh@gmail.com', '0048873920', '20-12-1999'),
+(23, 'Ánh', 'Nữ', 'Asnh@gmail.com', '0048873920', '20-12-1999'),
 (24, 'An', 'Nữ', 'An@gmail.com', '3301958187', '02-04-1991'),
-(25, 'Phương', 'Nữ', 'Phương@gmail.com', '3285472637', '26-07-2002'),
-(26, 'Thủy', 'Nữ', 'Thủy@gmail.com', '9334256333', '15-10-1995'),
+(25, 'Phương', 'Nữ', 'Phuong@gmail.com', '3285472637', '26-07-2002'),
+(26, 'Thủy', 'Nữ', 'Thuy@gmail.com', '9334256333', '15-10-1995'),
 (27, 'Thu', 'Nữ', 'Thu@gmail.com', '1858853935', '06-08-1998'),
 (28, 'Loan', 'Nữ', 'Loan@gmail.com', '7389927579', '31-03-2004'),
 (29, 'Hoa', 'Nữ', 'Hoa@gmail.com', '7517255793', '01-09-1996'),
@@ -304,36 +304,36 @@ CREATE TABLE `user_accounts` (
 
 INSERT INTO `user_accounts` (`id`, `userId`, `username`, `password`, `roleId`) VALUES
 (7, 1, 'a', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 1),
-(9, 7, 'test1', '123456', 1),
-(10, 2, 'test2', '123456', 1),
-(11, 3, 'test3', '123456', 2),
-(12, 4, 'test4', '123456', 2),
-(13, 5, 'test5', '123456', 3),
-(14, 6, 'test6', '123456', 3),
-(15, 8, 'test8', '123456', 4),
-(16, 9, 'test9', '123456', 4),
-(17, 10, 'test10', '123456', 5),
-(18, 11, 'test11', '123456', 5),
-(19, 12, 'test12', '123456', 6),
-(20, 13, 'test13', '123456', 6),
-(21, 14, 'test14', '123456', 6),
-(22, 15, 'test15', '123456', 6),
-(23, 16, 'test16', '123456', 6),
-(24, 17, 'test17', '123456', 6),
-(25, 18, 'test18', '123456', 6),
-(26, 19, 'test19', '123456', 6),
-(27, 20, 'test20', '123456', 6),
-(28, 21, 'test21', '123456', 6),
-(29, 22, 'test22', '123456', 6),
-(30, 23, 'test23', '123456', 6),
-(31, 24, 'test24', '123456', 6),
-(32, 25, 'test25', '123456', 6),
-(34, 27, 'test27', '123456', 6),
-(35, 28, 'test28', '123456', 6),
-(36, 29, 'test29', '123456', 6),
-(37, 30, 'test30', '123456', 6),
-(38, 31, 'test31', '123456', 6),
-(39, 32, 'test32', '123456', 6);
+(9, 7, 'test1', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 1),
+(10, 2, 'test2', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 1),
+(11, 3, 'test3', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 2),
+(12, 4, 'test4', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 2),
+(13, 5, 'test5', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 3),
+(14, 6, 'test6', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 3),
+(15, 8, 'test8', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 4),
+(16, 9, 'test9', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 4),
+(17, 10, 'test10', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 5),
+(18, 11, 'test11', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 5),
+(19, 12, 'test12', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 6),
+(20, 13, 'test13', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 6),
+(21, 14, 'test14', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 6),
+(22, 15, 'test15', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 6),
+(23, 16, 'test16', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 6),
+(24, 17, 'test17', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 6),
+(25, 18, 'test18', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 6),
+(26, 19, 'test19', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 6),
+(27, 20, 'test20', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 6),
+(28, 21, 'test21', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 6),
+(29, 22, 'test22', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 6),
+(30, 23, 'test23', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 6),
+(31, 24, 'test24', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 6),
+(32, 25, 'test25', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 6),
+(34, 27, 'test27', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 6),
+(35, 28, 'test28', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 6),
+(36, 29, 'test29', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 6),
+(37, 30, 'test30', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 6),
+(38, 31, 'test31', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 6),
+(39, 32, 'test32', '$2a$10$2pZEjV6A3rOIU8h.rU2zTe/eAR/wOIGM6vLkj28YNMj4ANqhipaqW', 6);
 
 -- --------------------------------------------------------
 
