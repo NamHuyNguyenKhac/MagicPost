446720fec69cd784bc90289e47e8c6e206c65d2e
Pham Minh <mhp12092003@gmail.com>
2023-12-28 18:15:22 +0700

    chia lai admin

---
+++ FrontEnd/src/components/Header/HeaderAdmin.js
@@ -0,0 +1,51 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTable, faUser, faChartSimple, faCircleUser, faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import './Header.css';
+import { useEffect, useState } from 'react';
+import { useNavigate } from "react-router-dom";
+
+function HeaderAdmin () {
+     //Chuyen route bang bien nay
+     const navigate = useNavigate();
+
+     //Xy ly nut tab
+     const handleForgotTabBtnHomeClick = () => {
+         navigate('/admin/Points');
+     }
+     const handleForgotTabBtnSearchClick = () => {
+         navigate('/admin/Chief');
+     }
+     const handleForgotTabBtnLoginClick = () => {
+         navigate('/profile');
+     }
+
+    return (
+        // {/* Header tab Dung lai cua forgotpassword*/}
+        <div className='headerTab'>
+            <div className='ForgotPasswordTabLogo'>
+                <div className='ForgotPasswordTabLogoImg'>
+                </div>
+                <div className='ForgotPasswordLogoText'>MagicPost</div>
+            </div>
+            {/* Nut Table */}   
+            <button className='ForgotPasswordTabBtn' onClick={handleForgotTabBtnHomeClick}>
+                <FontAwesomeIcon icon={faLocationDot} />
+                <div className='TextForgotPasswordTabBtn'>Points</div>
+            </button>
+            {/* Nut Search */}
+            <button className='ForgotPasswordTabBtn' onClick={handleForgotTabBtnSearchClick}>
+            <FontAwesomeIcon icon={faCircleUser} />
+                <div className='TextForgotPasswordTabBtn'>Leader</div>
+            </button>
+            {/* Nut Profile */}
+            <button className='ForgotPasswordTabBtnEnd' onClick={handleForgotTabBtnLoginClick}>
+                <FontAwesomeIcon icon={faUser} />
+                <div className='TextForgotPasswordTabBtn'>Profile</div>
+            </button>
+        </div>
+        // {/*  */}
+     )
+ 
+}
+
+export default HeaderAdmin;
