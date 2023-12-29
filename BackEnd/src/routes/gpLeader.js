a7805a102e08ee93e2079ecbf598291160992b3f
21020020daovuminhkhanh <100520334+21020020daovuminhkhanh@users.noreply.github.com>
2023-12-29 21:51:52 +0700

    a

--- BackEnd/src/routes/gpLeader.js
+++ BackEnd/src/routes/gpLeader.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const gpLeader = require("../controller/gpLeaderController");
 
 //"http://localhost:8080/gpLeader/..."
+//Quản lý nhân viên điểm tập kết 
 router.get("/getAllEmployeeInThisGP/:id", gpLeader.getAllEmployeeInThisGP);
 router.get("/createNewEmployee/:id/:fullname/:phoneNumber/:email/:sex/:username", gpLeader.createNewEmployee);
 
